1bdaf7be2f15a2c8940fca66a639a1d867634076
ziziting <125640208+ziziting@users.noreply.github.com>
2023-05-12 17:31:50 +0800

    Update ptarchive.py

--- ptarchive.py
+++ ptarchive.py
@@ -23,14 +23,14 @@ class DataPtArchive:
 
 
     def Data_Archive(self):
-        pt_cmd = f"""{ARCHIVE_TOOL} --source h={self.source_host},P={self.source_port},u={DUMP_USER},p={DUMP_PWD},D={self.db_name},t={self.tb_name},i={self.ix_name},A=utf8mb4  --dest h={self.archvie_host},P={self.archive_port},u={DUMP_USER},p={DUMP_PWD},D={self.dest_dbname},t={self.tb_name},A=utf8mb4 --progress {self.tnx} --sleep 1  --where "{self.condition_sql}" --statistics  --limit={self.tnx} --txn-size {self.tnx} --max-lag=5 --no-delete"""
+        pt_cmd = f"""{ARCHIVE_TOOL} --source h={self.source_host},P={self.source_port},u={DUMP_USER},p={DUMP_PWD},D={self.db_name},t={self.tb_name},i={self.ix_name},A=utf8mb4  --dest h={self.archvie_host},P={self.archive_port},u={DUMP_USER},p={DUMP_PWD},D={self.dest_dbname},t={self.tb_name},A=utf8mb4 --progress {self.tnx} --sleep 1  --where "{self.condition_sql}" --statistics  --limit={self.tnx} --txn-size {self.tnx} --max-lag=5 --ignor --retries 5 --no-delete"""
         exec_code, output = subprocess.getstatusoutput(pt_cmd)
         # if exec_code != 0:
         #     raise Exception(f"table {self.db_name}.{self.tb_name} dump failed. Error: {output}")
         return exec_code, output
 
     def Data_Purge(self):
-        pt_cmd = f"""{ARCHIVE_TOOL} --source h={self.source_host},P={self.source_port},u={DUMP_USER},p={DUMP_PWD},D={self.db_name},t={self.tb_name},i={self.ix_name},A=utf8mb4   --progress {self.tnx} --sleep 1  --where "{self.condition_sql}" --statistics  --limit={self.tnx} --txn-size {self.tnx} --max-lag=5 --purge """
+        pt_cmd = f"""{ARCHIVE_TOOL} --source h={self.source_host},P={self.source_port},u={DUMP_USER},p={DUMP_PWD},D={self.db_name},t={self.tb_name},i={self.ix_name},A=utf8mb4   --progress {self.tnx} --sleep 1  --where "{self.condition_sql}" --statistics  --limit={self.tnx} --txn-size {self.tnx} --max-lag=5 --retries 5 --purge """
         exec_code, output = subprocess.getstatusoutput(pt_cmd)
         return exec_code, output
 
